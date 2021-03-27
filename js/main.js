4509c1b4e5c1714ce6d0305757f465e0777438b6
bstrock <54157762+bstrock@users.noreply.github.com>
2021-03-26 23:07:35 -0500

    i found the attributes

--- js/main.js
+++ js/main.js
@@ -41,24 +41,46 @@ var generateMap = function(datasets) {
   let path = d3.geoPath() // define geopath generator
     .projection(projection); // asssign projection
 
+  let boroughsGeoJSON = topojson.feature(datasets.boroughs, datasets.boroughs.objects.london_boroughs).features;
+  console.log(boroughsGeoJSON)
   svg.selectAll('path') // create path object placeholders
     .attr('class', 'borough') // assign class
-    .data(topojson.feature(datasets.boroughs, datasets.boroughs.objects.london_boroughs).features) // feed d3
+    .data(boroughsGeoJSON) // feed d3
     .enter() // enter topology array
     .append('path') // append path to svg
     .attr('d', path) // assign path data to svg path
+    .attr('id', function(d){
+      return d.properties.NAME // tag name to path
+    })
     .style('fill', 'black') // classy night time fill
     .style('stroke', 'white') // classy night time outlines
     .style('stroke-width', '.25px'); // really really classy outlines
 
+  let centresGeoJSON = topojson.feature(datasets.centres, datasets.centres.objects.london_centres).features;
+
     svg.selectAll('path')  // create path object placeholders
     .attr('class', 'centre')  // assign class
-    .data(topojson.feature(datasets.centres, datasets.centres.objects.london_centres).features)  // feed d3
+    .data(centresGeoJSON)  // feed d3
     .enter()  // enter topology array
     .append('path')  // append path to svg
-    .attr('d', path);  // assign path data to svg path
+    .attr('d', path) // assign path data to svg path
+    .attr('id', function(d){
+      return d.properties.sitename  // tag sitename to path
+  });
+
+    let zoom = d3.zoom()
+      .scaleExtent([1, 8])
+      .on('zoom', function(e){
+        svg.selectAll('path')
+          .attr('transform', e.transform);
+      });
+
+    svg.call(zoom);
 
     let attributes = datasets.floorspace;
+
+
+
   colorize(attributes)
 
 };
